98a4a3bd8b0130b65f2f84cb01085d191b9a3582
LeHoaiVu <37165401+LeHoaiVu@users.noreply.github.com>
2018-07-02 13:33:26 +0700

    update

--- QuanLyGara/QuanLyGaraGUI/Properties/Settings.Designer.cs
+++ QuanLyGara/QuanLyGaraGUI/Properties/Settings.Designer.cs
@@ -8,22 +8,38 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace QuanLyGaraGUI.Properties
-{
-
-
+namespace QuanLyGaraGUI.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=.\\SQLEXPRESS;Initial Catalog=QuanLyGara;Integrated Security=True")]
+        public string QuanLyGaraConnectionString {
+            get {
+                return ((string)(this["QuanLyGaraConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-38ANKB5;Initial Catalog=QuanLyGara;Integrated Security=True")]
+        public string QuanLyGaraConnectionString1 {
+            get {
+                return ((string)(this["QuanLyGaraConnectionString1"]));
             }
         }
     }
